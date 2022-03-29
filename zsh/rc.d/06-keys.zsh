f4b5aa1b2c66d7678d2ec7b617711ede9dfe832f
Marlon Richert <marlonrichert@users.noreply.github.com>
2022-03-29 17:21:12 +0300

    Get rid of some custom keybindings in Zsh

--- zsh/rc.d/06-keys.zsh
+++ zsh/rc.d/06-keys.zsh
@@ -9,23 +9,6 @@ setopt NO_flowcontrol  # Enable ^Q and ^S.
 znap source marlonrichert/zsh-edit
 zstyle ':edit:*' word-chars '*?\'
 
-bind \
-    '^[p'   'cd .' \
-    '^[s'   'git status' \
-    '^[l'   'git log' \
-    '^[[5~' 'git push && git fetch upstream' \
-    '^[[6~' 'git fetch origin && git pull --autostash' \
-    '^[c'   'code .'
-code() {
-  command code ${@:/./${${$( git rev-parse --git-dir 2>/dev/null ):P:h}:-.}}
-}
-
-if [[ $VENDOR == apple ]]; then
-  bind '^[o' 'open .'
-else
-  bind '^[o' 'nautilus . &> /dev/null &|'
-fi
-
 # Replace some default keybindings with better built-in widgets.
 bindkey \
     '^[q'   push-line-or-edit \
